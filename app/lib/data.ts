c2613f399a0e7f2b7cf582c7b6aa4126ed9f26f6
Webi Muleta <133377877+Dirac1235@users.noreply.github.com>
2024-09-09 11:58:57 +0300

    feat: add validation, invoices, delete invoices

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,6 +1,6 @@
 import { sql } from '@vercel/postgres';
 import {
-  CustomerField,
+  CustomerField, 
   CustomersTableType,
   InvoiceForm,
   InvoicesTable,
@@ -14,9 +14,9 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+   
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
     // console.log('Data fetch completed after 3 seconds.');
